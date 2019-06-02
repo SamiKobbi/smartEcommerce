fcaee9635d16d3bab5bf93be29adee32b4d46f8a
unknown <samikobbi10@gmail.com>
2019-06-02 15:17:59 +0100

    SmartEcommerce project init

---
+++ src/main/java/com/smartApp/smartEcommerce/SmartEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.smartApp.smartEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEcommerceApplication.class, args);
+	}
+
+}
